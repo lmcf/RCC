bf62058539be36ff05ad0f7d06dc0bd601448539
Caldeiro, Luis M <caldeiro@protonmail.com>
2022-03-12 19:10:42 +0100

    v.0.0.0
    Preparación sistema LOGS

---
+++ main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 13:27:28 2022
+
+@author: calde
+"""
+
+from logger import Logs as L
+
+
+b = L('debug')
+b.msg("azucaar")
+
+a = L('error')
+
+a.msg("dasdas")
+
+
+
+
